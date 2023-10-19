427e882afe26a8dc3c762f4b82c646ca9d195260
techtitan007 <146939672+techtitan007@users.noreply.github.com>
2023-10-19 09:37:51 +0200

    Add files via upload

---
+++ human_rsrcs/humanresources/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [path('',views.home, name ='home')]
+
+urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
+from . import views
+
+urlpatterns = [
+    path('departments/create/', views.create_department, name='create_department'),
+    path('departments/<int:department_id>/update/', views.update_department, name='update_department'),
+    path('departments/<int:department_id>/delete/', views.delete_department, name='delete_department'),
+    path('employees/create/', views.create_employee, name='create_employee'),
+    path('employees/<int:employee_id>/update/', views.update_employee, name='update_employee'),
+    path('employees/<int:employee_id>/delete/', views.delete_employee, name='delete_employee'),
+]
+
+
+urlpatterns = [path('',views.home, name ='home')]
